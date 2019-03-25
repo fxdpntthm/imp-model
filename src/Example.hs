8550d2b5bda6c5bc9515bf51c9b7a87e8795757d
Apoorv Ingle <apoorv.ingle@gmail.com>
2019-03-25 16:55:29 -0500

    main is now example.hs

--- src/Example.hs
+++ src/Example.hs
@@ -1,25 +1,31 @@
 module Main where
 
 import System.IO
+import System.Environment
+import System.FilePath
+import System.Directory
 import Parser (imp, emptyState)
 import Model (generateModel)
 import Text.Parsec
 
 
 main :: IO ()
--- main = do writeFile "factorial.rs" (generateModel factorial)
---           writeFile "ite.rs" (generateModel factorial)
---           writeFile "unsatEx.rs" (generateModel unsatEx)
+main = do [f] <- getArgs
+          b <- doesFileExist f
+          if b then
+            (case (sanitize f) of
+               Left err -> error err
+               Right f -> do
+                 file <- readFile f
+                 case (runParser imp emptyState f file) of
+                   Left err -> putStrLn $ show err
+                   Right ast -> (do writeFile (replaceExtension f "rs") (generateModel ast)
+                                    putStrLn $ "z3 output to: " ++ (replaceExtension f "rs")))
+            else putStrLn $ "Error: File " ++ f ++ " does not exist"
 
 
-main = do file <- readFile "examples/factorial.imp"
-          case (runParser imp emptyState "examples/factorial.imp" file) of
-            Left err -> putStrLn $ show err
-            Right ast -> writeFile "examples/factorial.rs" (generateModel ast)
-          file <- readFile "examples/ite.imp"
-          case (runParser imp emptyState "examples/ite.imp" file) of
-            Left err -> putStrLn $ show err
-            Right ast -> writeFile "examples/ite.rs" (generateModel ast)
-                            
-           
           
+sanitize :: FilePath -> Either String FilePath
+sanitize f = if (".imp" /= takeExtension f)
+             then Left $ "Error: "++ f ++ " is not a valid Imp file. Imp files should have .imp extension"
+             else Right f                     
